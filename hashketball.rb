ed1d95c0896073f8989139e75c053991e85c6bd2
Dave Frame <dframe22@gmail.com>
2020-04-30 20:51:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,8 +9,8 @@ def game_hash
         [
           {
           player_name: "Alan Anderson", 
-          number: "0", 
-          shoe: "16", 
+          number: 0, 
+          shoe: 16, 
           points: "22", 
           rebounds: "12", 
           assists: "12", 
