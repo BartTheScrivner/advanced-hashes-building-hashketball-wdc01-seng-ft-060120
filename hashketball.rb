6a5400c14d76e0dc5a162656027420405d3dc900
Dave Frame <dframe22@gmail.com>
2020-04-30 20:35:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,33 @@
 # Write your code here!
+def game_hash do
+  game_hash = {
+    home: 
+      {
+      team_name: "Brooklyn Nets",
+      colors: ["black", "white"],
+      players: 
+        [
+          {},
+          {},
+          {},
+          {},
+          {}
+        ]
+      },
+    away: 
+      {
+      team_name: "Charlotte Hornets",
+      colors: ["turquoise", "purple"],
+      players: 
+        [
+          {},
+          {},
+          {},
+          {},
+          {}
+        ]
+      }
+  }
+  
+  game_hash
+end
